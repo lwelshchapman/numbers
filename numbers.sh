e49276a0dd5dd4ba2d8c59d264d88fee8b05916d
lwelshchapman <lwelsh@chapman.edu>
2019-11-13 10:48:50 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+# numbers.sh
+# Logan Welsh
+# CPSC-298-02
+
+# https://stackoverflow.com/questions/15867813/check-whether-one-number-equals-another-number-in-bash
+
+echo "Number: "
+read N
+I=1
+
+while [ $N -ne $((I-1)) ]
+do
+	if [ $((I%2)) -eq 0 ]
+	then
+		echo "$I Even"
+	else
+		echo "$I Odd"
+	fi
+	
+	I=$((I+1))
+done
